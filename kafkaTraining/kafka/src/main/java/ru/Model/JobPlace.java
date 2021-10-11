09e768e73edcc70c8f30ec0c89d570c2f7300b95
cam12-a <camara.alseny@yandex.ru>
2021-10-11 11:39:23 +0300

    atesta

--- kafkaTraining/kafka/src/main/java/ru/Model/JobPlace.java
+++
@@ -1,9 +0,0 @@
-package ru.Model;
-
-import org.apache.kafka.common.protocol.types.Field;
-
-public class JobPlace {
-    private String company_name;
-    private String date_from;
-    private String date_to;
-}
--- kafkaTraining/recruitment/src/main/java/ru/Model/JobPlace.java
+++
@@ -1,8 +0,0 @@
-package ru.Model;
-
-public class JobPlace {
-
-    private String company_name;
-    private String date_from;
-    private String date_to;
-}
